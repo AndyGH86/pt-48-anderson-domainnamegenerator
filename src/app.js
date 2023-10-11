7751f36235dc512f86bc882fb43b20c77cfc5a01
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2023-10-11 08:00:41 +0000

    creando domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const part1 = ["my", "the"];
+const part2 = ["great", "super"];
+const part3 = ["website", "project"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const inputElement = document.getElementById("input");
+const domainList = document.getElementById("domainList");
+const generateAndDisplayDomainNames = () => {
+  const input = inputElement.value;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  domainList.innerHTML = "";
+
+  let domainNames = [];
+
+  for (const p1 of part1) {
+    for (const p2 of part2) {
+      for (const p3 of part3) {
+        const domainName = `${p1}${p2}${input}${p3}.com`;
+        domainNames.push(domainName);
+
+        const listItem = document.createElement("li");
+        listItem.textContent = domainName;
+        domainList.appendChild(listItem);
+      }
+    }
+  }
 };
